bb173fc513a11be2a19e9db2bd30c6906196977f
兮尘 <761516186@qq.com>
2020-09-30 15:51:01 +0800

    Update score.py

--- score.py
+++ score.py
@@ -8,7 +8,12 @@ from scipy import spatial
 # lang = sys.argv[1]
 # w = float(sys.argv[2])
 lang = 'fr_en'
-w = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]  #
+# w = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]  #
+
+
+w = [0.7, 0.75, 0.8, 0.85, 0.9, 0.95]  #
+
+
 # w = [0.2, 0.5, 0.8]  #
 
 
@@ -18,6 +23,8 @@ class EAstrategy:
     kg1E = []
     kg2E = []
     EA_results = {}
+    SE_embedding = []
+    AE_embedding = []
 
     def read_EA_list(self, EAfile):
         # with open(EAfile,'r',encoding='utf-8') as r:
@@ -83,7 +90,34 @@ class EAstrategy:
             rline_list = rline.tolist()
             aline = ATElines[i].strip()
             aline_list = aline.split()
+            aline_list = [float(a) for a in aline_list]
             self.linkEmbedding.append(rline_list + aline_list)
+
+    def read_SE_AE(self, RTEembeddingfile, ATEembeddingfile):
+        self.SE_embedding = []
+        self.AE_embedding = []
+        RTElines = pickle.load(open(RTEembeddingfile, 'rb'), encoding='utf-8')
+        with open(ATEembeddingfile, 'r', encoding='utf-8') as r:
+            ATElines = r.readlines()
+        entlength = len(ATElines)
+        for i in range(entlength):  # 分配权重操作
+            rline = RTElines[i]
+            rline_list = rline.tolist()
+            rline_list_w = [float(j) for j in rline_list]
+            self.SE_embedding.append(rline_list_w)
+            aline = ATElines[i].strip()
+            aline_list = aline.split()
+            aline_list_w = [float(j) for j in aline_list]
+            self.AE_embedding.append(aline_list_w)
+
+    def EAlinkstrategy_weight_sim(self, w):
+        embedding = []
+        for i in range(len(self.SE_embedding)):
+            SE_w = [j * w for j in self.SE_embedding[i]]
+            AE_w = [j * (1 - w) for j in self.AE_embedding[i]]
+            add_weight = list(map(lambda x: x[0] + x[1], zip(SE_w, AE_w)))
+            embedding.append(add_weight)
+        return self.get_sim(embedding)
 
     def EAlinkstrategy_weight(self, RTEembeddingfile, ATEembeddingfile, w):
         self.linkEmbedding = []
@@ -126,25 +160,86 @@ class EAstrategy:
     #         if count % 10 == 0:
     #             print('w=' + str(w) + ' process:' + str(count) + '/' + str(len(self.kg1E)))
     #     print('complete distancing')
+    def EA_my_strategy(self, metric='euclidean'):
+        sim1 = self.get_sim(self.SE_embedding, metric)
+        sim2 = self.get_sim(self.AE_embedding, metric)
+        # print("SE")
+        # self.result(sim1)
+        # print("AE")
+        # self.result(sim2)
+        # print("AE+SE")
+        # self.result(sim1 + sim2)
+        sim_list = []
+        for ww in w:
+            print('AE+SE 权重策略 w=' + str(ww))
+            sim_list.append(ww * sim1 + (1 - ww) * sim2)
+        top_lr_list, top_rl_list = self.compute_batch_result(sim_list)
+        self.show_batch_result(top_lr_list, top_rl_list, [" %2.2f " % ww for ww in w])
+        # for cos_theta in (-1, -0.8, -0.5, -0.3, 0, 0.3, 0.5, 0.8, 1):
+        #     sim = np.sqrt(np.square(sim1) + np.square(sim2) - 2 * cos_theta * sim1 * sim2)
+        #     self.show_result(sim)
 
-    def get_hits(self, top_k=(1, 10, 50, 100)):
+    def get_sim(self, embedding_matrix, metric='cityblock'):
+        Lvec = np.array([embedding_matrix[e1] for e1, e2 in self.seeds])
+        Rvec = np.array([embedding_matrix[e2] for e1, e2 in self.seeds])
+        sim = spatial.distance.cdist(Lvec, Rvec, metric=metric)
+        return sim
+
+    def get_hits(self, top_k=(1, 10, 50, 100), metric='cityblock'):
         Lvec = np.array([self.linkEmbedding[e1] for e1, e2 in self.seeds])
         Rvec = np.array([self.linkEmbedding[e2] for e1, e2 in self.seeds])
-        sim = spatial.distance.cdist(Lvec, Rvec, metric='cityblock')
+        sim = spatial.distance.cdist(Lvec, Rvec, metric=metric)
+        self.result(sim, top_k)
+
+    def compute_batch_result(self, sim_list, top_k=(1, 10, 50, 100)):
+        top_lr_list = []
+        top_rl_list = []
+        for sim in sim_list:
+            top_lr, top_rl = self.compute_result(sim, top_k)
+            top_lr_list.append(top_lr)
+            top_rl_list.append(top_rl)
+        return top_lr_list, top_rl_list
+
+    def compute_result(self, sim, top_k=(1, 10, 50, 100)):
         top_lr = [0] * len(top_k)
-        for i in range(Lvec.shape[0]):  # 对于每个KG1实体
+        for i in range(len(self.seeds)):  # 对于每个KG1实体
             rank = sim[i, :].argsort()
             rank_index = np.where(rank == i)[0][0]
             for j in range(len(top_k)):
                 if rank_index < top_k[j]:
                     top_lr[j] += 1
         top_rl = [0] * len(top_k)
-        for i in range(Rvec.shape[0]):
+        for i in range(len(self.seeds)):
             rank = sim[:, i].argsort()
             rank_index = np.where(rank == i)[0][0]
             for j in range(len(top_k)):
                 if rank_index < top_k[j]:
                     top_rl[j] += 1
+        return top_lr, top_rl
+
+    def result(self, sim, top_k=(1, 10, 50, 100)):
+        top_lr, top_rl = self.compute_result(sim, top_k)
+        self.show_result(top_lr, top_rl, top_k)
+
+    def show_batch_result(self, top_lr_list, top_rl_list, titles=(), top_k=(1, 10, 50, 100)):
+        print("          ", end="")
+        for i in titles:
+            print(i, end="")
+        print()
+        print('For each left:')
+        for i in range(len(top_k)):
+            print('Hits@%3d: ' % (top_k[i]), end="")
+            for top_lr in top_lr_list:
+                print('%.2f%% ' % (top_lr[i] / len(self.seeds) * 100), end="")
+            print()
+        print('For each right:')
+        for i in range(len(top_k)):
+            print('Hits@%3d: ' % (top_k[i]), end="")
+            for top_rl in top_rl_list:
+                print('%.2f%% ' % (top_rl[i] / len(self.seeds) * 100), end="")
+            print()
+
+    def show_result(self, top_lr, top_rl, top_k=(1, 10, 50, 100)):
         print('For each left:')
         for i in range(len(top_lr)):
             print('Hits@%d: %.2f%%' % (top_k[i], top_lr[i] / len(self.seeds) * 100))
@@ -162,22 +257,29 @@ test.read_KG1_and_KG2_list('data/' + lang + '/ent_ids_1', 'data/' + lang + '/ent
 
 print('language:' + lang)
 
-struct_embedding = 'result/' + lang + '/RTentsembed.pkl'
+struct_embedding = 'result/test0/' + lang + '/RTentsembed.pkl'
 # attribute_embedding = 'result/' + lang + '/ATentsembed.txt'
-attribute_embedding = 'result/TransE2/fr_en/ATentsembed.txt'
-print('拼接策略')
+attribute_embedding = 'result/test3/ATentsembed.txt_score_44'
+test.read_SE_AE(struct_embedding, attribute_embedding)
+# print('拼接策略')
 # 拼接策略
-test.EAlinkstrategy(struct_embedding, attribute_embedding)  # 连接策略
+# test.EAlinkstrategy(struct_embedding, attribute_embedding)  # 连接策略
 # test.EAlinkstrategy('2.pkl', './result/ja_en/ATentsembed.txt')  # 连接策略
-test.get_hits()
+# test.get_hits(metric='cityblock')
+
+print('my策略')
+# 拼接策略
+test.EA_my_strategy("cityblock")
 
 # 权重策略
 # ww = 0.8
 # # test.EAlinkstrategy_weight('data/'+lang+'/RTentsembed.pkl','data/'+lang+'/ATentsembed.txt', ww) #连接策略
-# for ww in w:
-#     print('权重策略 w=' + str(ww))
-#     test.EAlinkstrategy_weight(struct_embedding, attribute_embedding, ww)  # 连接策略
-#     test.get_hits()
+sim_list = []
+for ww in w:
+    print('AE+SE 权重策略 w=' + str(ww))
+    sim_list.append(test.EAlinkstrategy_weight_sim(ww))
+top_lr_list, top_rl_list = test.compute_batch_result(sim_list)
+test.show_batch_result(top_lr_list, top_rl_list, ["  %2.2f  " % ww for ww in w])
 
 # 迭代策略
 # test.EAlinkstrategy_iteration('results/'+'emb_it_'+lang+'.pkl')
@@ -187,15 +289,15 @@ test.get_hits()
 #     w.write('\n')
 
 # 消融实验
-print("关系消融实验")
-test.XRR(struct_embedding)
-test.get_hits()
+# print("关系消融实验")
+# test.XRR(struct_embedding)
+# test.get_hits()
 
-print("属性消融实验")
-test.XRA(attribute_embedding)
+# print("属性消融实验")
+# test.XRA(attribute_embedding)
 # test.XRA('./result/ja_en/ATentsembed.txt')
 # test.XRA('res/entity2vec.bern')
-test.get_hits()
+# test.get_hits()
 
 # 迭代权重策略
 # test.EAlinkstrategy_iteration('results/'+'emb_itwe_0.5_'+lang+'.pkl')
